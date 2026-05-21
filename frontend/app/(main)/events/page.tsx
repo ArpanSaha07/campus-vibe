bde1b3ec75fec616e50ac7686ad50961eadb098f
Arpan Saha <sahaarpan550@gmail.com>
2026-05-20 21:27:33 -0400

    Updated EventGrid.tsx component to be used in the /events page

--- frontend/app/(main)/events/page.tsx
+++ frontend/app/(main)/events/page.tsx
@@ -2,7 +2,7 @@
 import { useEffect, useState } from "react";
 import { listEvents } from "@/app/lib/event"
 import { EventInstance } from "@/app/types";
-import EventSection from "@/app/components/main-page/EventSectionMainPage";
+import EventGrid from "@/app/components/event/EventGrid";
 
 import { popularEvents } from "@/app/data/data"; // Temporary hardcoded events until backend is ready
 
@@ -16,12 +16,11 @@ export default function DiscoverEvents() {
 
   return (
     <div className="p-6 bg-white rounded-md shadow-md hover:shadow-lg transition">
-      <div className="px-12">
+      <div className="px-8">
         <h2 className="text-xl font-semibold mb-4">Discover Events</h2>
         <p className="text-gray-600">Explore upcoming events on campus and join the fun!</p>
       </div>
-
-      <EventSection title="Upcoming Events" events={popularEvents} />
+      <EventGrid events={popularEvents} />
     </div>
   );
 }
